722902bd47d2d7ec6cc9aa57448567b084a13736
partharoyy <69928055+partharoyy@users.noreply.github.com>
2024-06-27 23:51:18 +0530

    Add profile screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Jumanji!</Text>
       <StatusBar style='auto' />
+      <Link href='/profile'>Go to Profile</Link>
     </View>
   );
 }
