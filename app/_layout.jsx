eef51f8a41ee0804b146536da28206ba0ac50f4a
partharoyy <69928055+partharoyy@users.noreply.github.com>
2024-06-30 00:36:22 +0530

    Complete SignUp, SignIn screens

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -28,17 +28,11 @@ const RootLayout = () => {
   return (
     <Stack>
       <Stack.Screen name='index' options={{ headerShown: false }} />
+      <Stack.Screen name='(auth)' options={{ headerShown: false }} />
+      <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+      {/* <Stack.Screen name='/search/[query]' options={{ headerShown: false }} /> */}
     </Stack>
   );
 };
 
 export default RootLayout;
-
-const styles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
